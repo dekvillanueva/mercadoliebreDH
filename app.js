0b0a7b915f4ebf3a3ef606c2bf3c30d32f2bc688
@dekvillanueva <dekvillanueva@gmail.com>
2022-05-22 18:23:36 -0300

    Se crean y modifican los archivos login.html, register.html y sus estilos loginStyle.css y registerStyle. Se modifica app.js, home.html y homeStyle.css para cumplir con los micro desafios de la Clase 15

--- app.js
+++ app.js
@@ -12,19 +12,31 @@ const public = path.join(__dirname, 'public/');
 // HTTP routes
 const httpRaiz = '/';
 const httpHome ='/home';
+const httpRegister ='/register';
+const httpLogin ='/login';
 
 // HTML
 const homeHtml = 'home.html';
+const registerHtml = 'register.html';
+const loginHtml = 'login.html';
 
 // Define the static file path
 app.use(express.static(__dirname + '/public/'));
 
 app.get(httpRaiz, (req, res) => {
-    res.sendFile(path.join(views, homeHtml))
+    res.sendFile(path.join(views, homeHtml));
 });
 
 app.get(httpHome, (req,res) =>{
-    res.sendFile(path.join(views, homeHtml))
+    res.sendFile(path.join(views, homeHtml));
+});
+
+app.get(httpRegister, (req,res) =>{
+    res.sendFile(path.join(views, registerHtml));
+});
+
+app.get(httpLogin, (req,res) =>{
+    res.sendFile(path.join(views, loginHtml));
 });
 
 app.listen(port, () =>{
